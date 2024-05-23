5dc54d17f048eccea8cb7064a38f498e50877da7
boyan4975 <166320049+boyan4975@users.noreply.github.com>
2024-05-23 03:31:13 -0700

    Update chart3.R

--- chart3.R
+++ chart3.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(dplyr)
 
 # Temperature Change vs. Latitude
-ggplot(population_climate_1900_2010, aes(x = LAT, y = Temp_Change)) +
+tlat_chart <- ggplot(population_climate_1900_2010, aes(x = LAT, y = Temp_Change)) +
   geom_point(aes(color = Region), alpha = 0.6) +
   labs(title = "Temperature Change vs. Latitude",
        x = "Latitude",
@@ -10,7 +10,7 @@ ggplot(population_climate_1900_2010, aes(x = LAT, y = Temp_Change)) +
   theme_minimal()
 
 # Precipitation Change vs. Latitude
-ggplot(population_climate_1900_2010, aes(x = LAT, y = Prcp_Change)) +
+plat_chart <- ggplot(population_climate_1900_2010, aes(x = LAT, y = Prcp_Change)) +
   geom_point(aes(color = Region), alpha = 0.6) +
   labs(title = "Precipitation Change vs. Latitude",
        x = "Latitude",
@@ -18,7 +18,7 @@ ggplot(population_climate_1900_2010, aes(x = LAT, y = Prcp_Change)) +
   theme_minimal()
 
 # Temperature Change vs. Longitude
-ggplot(population_climate_1900_2010, aes(x = LON, y = Temp_Change)) +
+tlon_chart <- ggplot(population_climate_1900_2010, aes(x = LON, y = Temp_Change)) +
   geom_point(aes(color = Region), alpha = 0.6) +
   labs(title = "Temperature Change vs. Longitude",
        x = "Longitude",
@@ -26,7 +26,7 @@ ggplot(population_climate_1900_2010, aes(x = LON, y = Temp_Change)) +
   theme_minimal()
 
 # Scatter plot for Precipitation Change vs. Longitude
-ggplot(population_climate_1900_2010, aes(x = LON, y = Prcp_Change)) +
+plon_chart <- ggplot(population_climate_1900_2010, aes(x = LON, y = Prcp_Change)) +
   geom_point(aes(color = Region), alpha = 0.6) +
   labs(title = "Precipitation Change vs. Longitude",
        x = "Longitude",
